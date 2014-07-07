cbe6fc8007c1d2b923bbeef31163c903b7f15ea8
elisechant <elise.chant@gmail.com>
2014-07-08 06:51:44 +1000

    add remove vote logic

--- src/scripts/routes/application_route.js
+++ src/scripts/routes/application_route.js
@@ -44,9 +44,70 @@ App.ApplicationRoute = Ember.Route.extend({
 			});
 		},
 
-
+		/**
+		 * Remove a vote
+		 * Assume that if that if an issue is checked, I have already
+		 * voted for it
+		 * @param issue {object}
+		 */
 		removeVote: function(issue) {
-			console.log('remove vote')
+			console.log('remove vote');
+
+			var self = this,
+				currentUser = self.get('controller').get('currentUser').get('content'),
+				currentIssue = issue.get('content'),
+				currentVote = '';
+
+
+//			i am a user
+//			i am checking an issue
+//			if issue is checked I have already selected it ,
+//				find vote by user id, delete the issue
+//			if not i will createRecord
+
+			var usersWhoVotesOnIssue = issue.get('content').get('votes').getEach('user.id');
+
+			for (var i=0; i<usersWhoVotesOnIssue.length; i++) {
+				if (currentUser.get('id') === usersWhoVotesOnIssue[i]) {
+					// vote index of matched users
+					var voteIdInContext = issue.get('content').get('votes').getEach('id')[i];
+
+					currentVote = self.get('store').find('vote', voteIdInContext).then(function(rec) {
+						rec.deleteRecord();
+						rec.save();
+
+						currentIssue.get('votes').removeObject(rec);
+						currentUser.get('votes').removeObject(rec);
+					});
+				}
+			}
+
+
+
+//			if (currentVote.length) {
+//				currentVote.deleteRecord();
+//				currentVote.save();
+//			}
+
+
+//			var currentVoteIdsByIssue = currentIssue.get('votes').mapBy('id');
+//			var currentVoteIdsByUser = currentUser.get('votes').mapBy('id');
+//
+//			for (var i=1; i<currentVoteIdsByIssue.length; i++) {
+//				// is a match
+//				if (currentVoteIdsByUser.indexOf(currentVoteIdsByIssue[i]) > -1) {
+//					// determined vote in context
+//					currentVote = self.get('store').find('vote', currentVoteIdsByIssue[i]).then(function(vote) {
+//						vote.deleteRecord();
+//
+//					});
+//				}
+//			}
+//
+//			currentUser = '';
+//			currentIssue = '';
+//			currentVote = '';
+
 		},
 
 
